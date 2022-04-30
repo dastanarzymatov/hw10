5eea3f4fc9b1f64c3845369c2ae9e1cd3e95488e
dasta <arzymatodastan@gmail.com>
2022-04-30 19:03:05 +0600

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(get(scanner.nextInt(), 3, 4, 5, 6, 7, 8, 9, 10));
+
+    }
+
+    public static boolean get(int a, int... array) {
+        for (int i = 0; i < array.length; i++) {
+            if (a == array[i]) {
+
+                return true;
+
+            } else {
+
+            }
+        }
+        return false;
     }
 }
+
+
